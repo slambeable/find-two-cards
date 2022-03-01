5155719bd2de74ab130da8c4fbdb153793e6e6eb
slambeable <41022916+slambeable@users.noreply.github.com>
2022-03-01 06:02:36 +0300

    add action for development button

--- FindTwoCards/Controllers/Screens/StartScreenViewController.swift
+++ FindTwoCards/Controllers/Screens/StartScreenViewController.swift
@@ -13,20 +13,11 @@ class StartScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setStylesForStartButtons()
     }
-
-    private func setStylesForStartButtons() {
-//        for button in startScreenButtons {
-//            button.layer.cornerRadius = 12
-//            button.layer.borderColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5).cgColor
-//            button.layer.borderWidth = 2
-//            
-//            button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
-//            button.layer.shadowOpacity = 1
-//            button.layer.shadowOffset = CGSize(width: 0, height: 4)
-//            button.layer.shadowRadius = 4
-//        }
+    
+    
+    @IBAction func showPopUp() {
+        // Тут вызов попапа
     }
 }
 
